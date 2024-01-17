c58ebafcd6bded539029f59a181b1e55752b0454
iscemmanuelitd <84645982+iscemmanuelitd@users.noreply.github.com>
2024-01-17 02:43:54 -0600

    Actualizar index.js
    rearmar API Autenticar

--- BD/index.js
+++ BD/index.js
@@ -1,53 +1,38 @@
-/*
-const { MongoClient} = require('mongodb');
-const uri ="mongodb+srv://corporativo723:Durango723@cluster0.82r1d65.mongodb.net/"
-const client = new MongoClient(uri)
-*/
 
 var axios = require('axios');
-/*
-const {Realm} = require("realm-web")
-const app = new Realm.App ({ id: 'data-rsgsu' });
-*/
 
 
-const BD  = {
-    /*
-             conectar:async (_db)=>{await client.connect(); console.log("Base de Datos Conectada Exitosamente"); db=client.db(_db)},
-            consultar:async (_col)=>{return await db.collection(_col).find().toArray()},
-            insertar:async (_col,_dat)=>{await db.collection(_col).insertOne(_dat)},
-            */
-            autenticar:(_dat)=>{return auten(_dat)}
-            }
-            
-function auten(_dat){
-    var data = JSON.stringify({collection: "usuarios",database: "db723",dataSource: "Cluster0",filter:_dat,projection:{_id:0}});
-    var config = {method: 'post',
-        url: 'https://us-east-2.aws.data.mongodb-api.com/app/data-rsgsu/endpoint/data/v1/action/findOne',
+_d = ["post","findOne","db723","usuarios",{nombre:$usu, contra: $pass}]
+
+const serviciosWeb = {
+            autenticar:(_d)=>{return auten(_d[0],_d[1], _d[2],_d[3],_d[4])},
+            buscar:(_dat)=>{  },
+            insertar:(_dat) => {  },
+            modificar:(_dat) => {  },
+            ubicar:(_dat) => { }
+       }   
+
+
+let datosApi = function(_db,_co,_fil,_pr={_id:0}){
+      return JSON.stringify({collection: _co, database: _db, dataSource: "Cluster0",filter:_fil,projection:_pr})
+}
+
+let con = (_mh,_url,db,con,fil)=>{
+    return {method: _mh, url: `https://us-east-2.aws.data.mongodb-api.com/app/data-rsgsu/endpoint/data/v1/action/${_url}`,
         headers: {'Content-Type': 'application/json','Access-Control-Request-Headers': '*','api-key': 'AMO2GjAOfZYRiO1VDIzdVnejibVfO3bdvGjRUpqPTaCJTGjYHw4PGx8mZbqpb6xK',},
-        data: data}
-    return axios(config).then(function (response) {
-            console.log(JSON.stringify(response.data.document));
-            return [response.data.document]
+        data: datosApi(db,co,fil)}
+}
+
+
+let auten = (_mh,_url,db,con,fil){
+    return axios(con(mh,_url,db,con,fil))
+    .then(function (response) {
+            let _dat = response.data.document
+            return [_dat]
         }).catch(function (error) {
-            console.log(error);
+            return [error]
         });
 
 }
 
-
-/*
-  
-async function loginEmailPassword(email, password) {
-  const credentials = Realm.Credentials.emailPassword(email, password);
-  const user = await app.logIn(credentials);
-  console.assert(user.id === app.currentUser.id);
-  return user;
-}
-  
-*/
-
-
-
-
-module.exports=BD
+module.exports=serviciosWeb 
