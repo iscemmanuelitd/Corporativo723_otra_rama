ec3167294e8ad48aac13278ee777067e25e19d7e
iscemmanuelitd <84645982+iscemmanuelitd@users.noreply.github.com>
2024-01-17 08:53:20 -0600

    Actualizar mapGoogle.js
    Coordenadas

--- public/htmls/mapGoogle.js
+++ public/htmls/mapGoogle.js
@@ -3,7 +3,7 @@ let map;
 
 async function initMap() {
   // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
+  const position = { lat: 22.22344, lng: -102.031 };
   // Request needed libraries.
   //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
